90b9918095a07d50f2673b3d212b3606039993d9
Zeynep Kılıç <18zeynepklc@gmail.com>
2026-02-10 21:28:33 -0800

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Zeynep Kılıç on 10.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
